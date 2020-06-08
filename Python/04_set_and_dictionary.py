5cddb706699d589de9ebf2f3f8af4c432db657bf
sehooh5 <sehooh5@google.com>
2020-06-08 14:29:34 +0900

    update 04 and add DataType.md

--- Python/04_set_and_dictionary.py
+++ Python/04_set_and_dictionary.py
@@ -12,4 +12,36 @@ print(set_a - set_b) # 차집합
 print(set_a | set_b) # 합집합
 print(set_a & set_b) # 교집합
 
+set_c = set()
+
 set_c = {1,1,1} # {1}
+
+
+## 2. dictionary
+# dictionary는 key 와 value가 쌍으로 이루어져 있으며, 궁극의 자료구조라 한다. (JSON 과 동일한 형태)
+# key 중복이 되어선 안되며 중복되면 나중에 작성된 key 값으로 설정된다
+# key 는 불변한 모든 것이 가능. immutable = (str, int, float, boolean, tuple, range)
+# value 는 list, dict 를 포함한 모든 타입이 가능하다
+
+# dictionary 생성방법
+dict_a = {}
+dict_b = dict()
+print(type(dict_a))
+print(type(dict_b))
+
+dict_a = {1:1 ,2: 2,3: 3, 'c': 4}
+print(dict_a)
+
+phone_book = {
+    '서울' : '02',
+    '경기' : '031',
+    '대전' : '042'
+    }
+print(phone_book['서울'])
+
+# 실행 가능한 함수 보여주는 함수 : dir()
+print(dir(dict))
+# 공식문서의 설명 내용을 보여줌 : help
+print(help(dict))
+
+print(phone_book.values()) # =dict_values(['02','031','041']) : type = dict_values but [] 이므로 list처럼 사용
---
+++ Python/DataType.md
@@ -0,0 +1,15 @@
+# Data type
+
+### Sequence(Ordered)
+
+- `string`
+- `list` : 가변
+- `tuple`
+- `range()`
+
+
+
+### Unordered
+
+- `set` : 가변
+- `dictionary` : 가변, but `key` 는 불변
